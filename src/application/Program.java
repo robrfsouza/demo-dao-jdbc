3cb8c16c75938b420594a93d812ef08839715d85
robrfsouza <robrfs@gmail.com>
2024-02-20 01:55:06 -0300

    Seller entitie class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,16 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Department department = new Department(1, "Book");
-		System.out.println(department);
+		
+		Seller seller = new Seller (21, "Bobo", "bobo@gmail.com", new Date(), 3000.00, department);
+		System.out.println(seller);
 	}
 }
