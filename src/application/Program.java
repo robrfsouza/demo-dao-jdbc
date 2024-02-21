b87b86b330437edddc8728da1c21c8b9b8cc2030
robrfsouza <robrfs@gmail.com>
2024-02-21 20:58:04 -0300

    DepartmentDao and SellerDao interfaces

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,6 +11,7 @@ public class Program {
 		Department department = new Department(1, "Book");
 		
 		Seller seller = new Seller (21, "Bobo", "bobo@gmail.com", new Date(), 3000.00, department);
+		
 		System.out.println(seller);
 	}
 }
